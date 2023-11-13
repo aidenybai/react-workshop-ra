939355d3258dcaff81fb00e2b004c39fcdfb6561
Aiden Bai <aiden.bai05@gmail.com>
2023-11-13 12:41:57 +0000

    auto-optimize: widgets-editor/src/components/designSystems/blueprint/CloseButton.tsx

--- widgets-editor/src/components/designSystems/blueprint/CloseButton.tsx
+++ widgets-editor/src/components/designSystems/blueprint/CloseButton.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { Color } from "constants/Colors";
 import { Button } from "@blueprintjs/core";
+
 type CloseButtonProps = {
   color: Color;
   size: number;
@@ -17,16 +18,18 @@ const StyledButton = styled(Button)<CloseButtonProps>`
   padding: 0;
   color: ${(props) => props.color};
   & svg {
-    width: ${(props) => props.size};
-    height: ${(props) => props.size};
+    width: ${(props) => props.size}px;
+    height: ${(props) => props.size}px;
   }
 `;
 
-export const CloseButton = (props: CloseButtonProps) => {
+export const CloseButton: React.FC<CloseButtonProps> = (props) => {
   return (
     <StyledButton
       className={props.className}
-      {...props}
+      onClick={props.onClick}
+      color={props.color}
+      size={props.size}
       rightIcon="cross"
       minimal
     />
