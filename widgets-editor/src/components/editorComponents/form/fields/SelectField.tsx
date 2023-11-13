5af107b022c2c25d026566496a9e28dc7b5c83bc
Aiden Bai <aiden.bai05@gmail.com>
2023-11-13 12:46:04 +0000

    auto-optimize: widgets-editor/src/components/editorComponents/form/fields/SelectField.tsx

--- widgets-editor/src/components/editorComponents/form/fields/SelectField.tsx
+++ widgets-editor/src/components/editorComponents/form/fields/SelectField.tsx
@@ -1,23 +1,6 @@
 import React from "react";
-import {
-  Field,
-  WrappedFieldMetaProps,
-  WrappedFieldInputProps,
-} from "redux-form";
+import { Field, WrappedFieldMetaProps, WrappedFieldInputProps } from "redux-form";
 import DropdownWrapper from "./DropdownWrapper";
-
-const renderComponent = (
-  componentProps: SelectFieldProps & {
-    meta: Partial<WrappedFieldMetaProps>;
-    input: Partial<WrappedFieldInputProps>;
-  },
-) => {
-  return (
-    <React.Fragment>
-      <DropdownWrapper {...componentProps} />
-    </React.Fragment>
-  );
-};
 
 type SelectFieldProps = {
   name: string;
@@ -27,15 +10,23 @@ type SelectFieldProps = {
   outline?: boolean;
 };
 
+const renderComponent = (componentProps: SelectFieldProps & {
+  meta: Partial<WrappedFieldMetaProps>;
+  input: Partial<WrappedFieldInputProps>;
+}) => {
+  return (
+    <React.Fragment>
+      <DropdownWrapper {...componentProps} />
+    </React.Fragment>
+  );
+};
+
 export const SelectField = (props: SelectFieldProps) => {
   return (
     <Field
       name={props.name}
-      placeholder={props.placeholder}
       component={renderComponent}
-      options={props.options}
-      size={props.size}
-      outline={props.outline}
+      props={props}
     />
   );
 };
