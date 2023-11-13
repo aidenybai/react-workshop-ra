f538bbd404cb68f010f5b829c5aa6efbdbc3a9c9
Aiden Bai <aiden.bai05@gmail.com>
2023-11-13 12:42:34 +0000

    auto-optimize: widgets-editor/src/components/designSystems/appsmith/IconComponent.tsx

--- widgets-editor/src/components/designSystems/appsmith/IconComponent.tsx
+++ widgets-editor/src/components/designSystems/appsmith/IconComponent.tsx
@@ -2,33 +2,34 @@ import React from "react";
 import { Icon, Intent } from "@blueprintjs/core";
 import { IconName } from "@blueprintjs/icons";
 import { noop } from "utils/AppsmithUtils";
+
 export type IconType = IconName | string;
 
-class IconComponent extends React.Component<IconComponentProps> {
-  render() {
-    return (
-      <Icon
-        style={{
-          cursor:
-            this.props.onClick && !this.props.disabled ? "pointer" : "auto",
-        }}
-        icon={this.props.iconName as IconName}
-        iconSize={this.props.iconSize}
-        intent={this.props.intent}
-        onClick={this.props.disabled ? noop : this.props.onClick}
-        color={this.props.color}
-      />
-    );
-  }
-}
-
-export interface IconComponentProps {
+interface IconComponentProps {
   iconSize?: number;
   iconName?: IconType;
   intent?: Intent;
   disabled?: boolean;
   onClick?: () => void;
   color: string;
+}
+
+class IconComponent extends React.Component<IconComponentProps> {
+  render() {
+    const { iconSize, iconName, intent, disabled, onClick, color } = this.props;
+    const cursorStyle = onClick && !disabled ? "pointer" : "auto";
+
+    return (
+      <Icon
+        style={{ cursor: cursorStyle }}
+        icon={iconName as IconName}
+        iconSize={iconSize}
+        intent={intent}
+        onClick={disabled ? noop : onClick}
+        color={color}
+      />
+    );
+  }
 }
 
 export default IconComponent;
