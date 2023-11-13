5e28ab460824b6d1e78221af5a60f62b8e4438c2
Aiden Bai <aiden.bai05@gmail.com>
2023-11-13 12:46:50 +0000

    auto-optimize: widgets-editor/src/components/editorComponents/GlobalSearch/useRecentEntities.tsx

--- widgets-editor/src/components/editorComponents/GlobalSearch/useRecentEntities.tsx
+++ widgets-editor/src/components/editorComponents/GlobalSearch/useRecentEntities.tsx
@@ -5,52 +5,41 @@ import { getActions, getAllWidgetsMap } from "selectors/entitiesSelector";
 import { SEARCH_ITEM_TYPES } from "./utils";
 import { get } from "lodash";
 
-const recentEntitiesSelector = (state: AppState) =>
-  state.ui.globalSearch.recentEntities;
-
-const useResentEntities = () => {
+const useRecentEntities = () => {
   const widgetsMap = useSelector(getAllWidgetsMap);
-  let recentEntities = useSelector(recentEntitiesSelector);
-  recentEntities = recentEntities.slice(1);
+  const recentEntities = useSelector((state: AppState) => state.ui.globalSearch.recentEntities.slice(1));
   const actions = useSelector(getActions);
-  const reducerDatasources = useSelector((state: AppState) => {
-    return state.entities.datasources.list;
-  });
-
+  const reducerDatasources = useSelector((state: AppState) => state.entities.datasources.list);
   const pages = useSelector(getPageList) || [];
 
-  const populatedRecentEntities = recentEntities
-    .map((entity) => {
-      const { type, id, params } = entity;
-      if (type === "page") {
-        const result = pages.find((page) => page.pageId === id);
-        if (result) {
-          return {
-            ...result,
-            kind: SEARCH_ITEM_TYPES.page,
-          };
-        } else {
-          return null;
-        }
-      } else if (type === "datasource") {
-        const datasource = reducerDatasources.find(
-          (reducerDatasource) => reducerDatasource.id === id,
-        );
-        return (
-          datasource && {
-            ...datasource,
-            pageId: params?.pageId,
-          }
-        );
-      } else if (type === "action")
-        return actions.find((action) => action?.config?.id === id);
-      else if (type === "widget") {
-        return get(widgetsMap, id, null);
+  const populatedRecentEntities = recentEntities.map((entity) => {
+    const { type, id, params } = entity;
+    if (type === "page") {
+      const result = pages.find((page) => page.pageId === id);
+      if (result) {
+        return {
+          ...result,
+          kind: SEARCH_ITEM_TYPES.page,
+        };
+      } else {
+        return null;
       }
-    })
-    .filter(Boolean);
+    } else if (type === "datasource") {
+      const datasource = reducerDatasources.find((reducerDatasource) => reducerDatasource.id === id);
+      return (
+        datasource && {
+          ...datasource,
+          pageId: params?.pageId,
+        }
+      );
+    } else if (type === "action") {
+      return actions.find((action) => action?.config?.id === id);
+    } else if (type === "widget") {
+      return get(widgetsMap, id, null);
+    }
+  }).filter(Boolean);
 
   return populatedRecentEntities;
 };
 
-export default useResentEntities;
+export default useRecentEntities;
