c6b30d02b64dea430744669f76ceedcfa6f5795f
Aiden Bai <aiden.bai05@gmail.com>
2023-11-13 07:39:17 +0000

    auto-optimize: notes/src/components/NotesList/index.jsx

--- notes/src/components/NotesList/index.jsx
+++ notes/src/components/NotesList/index.jsx
@@ -4,40 +4,38 @@ import FilterInput from "../FilterInput";
 import NoteButton from "../NoteButton";
 import "./index.css";
 
-function NotesList({
+const NotesList = ({
   notes,
   activeNoteId,
   onNoteActivated,
   onNewNotesRequested,
   onDeleteAllRequested,
-}) {
+}) => {
   const [filter, setFilter] = useState("");
+  const noteCount = Object.keys(notes).length;
+  const sortedNotes = Object.values(notes).sort((a, b) => b.date.getTime() - a.date.getTime());
+
+  const handleNoteActivation = (id) => () => onNoteActivated(id);
+  const handleNewNoteRequest = (count, paragraphs) => () => onNewNotesRequested({ count, paragraphs });
 
   return (
-    <div className="notes-list" style={{ position: "relative" }}>
+    <div className="notes-list">
       <div className="notes-list__filter">
         <FilterInput
           filter={filter}
           onChange={setFilter}
-          noteCount={Object.keys(notes).length}
+          noteCount={noteCount}
         />
       </div>
 
       <div className="notes-list__notes">
-        {Object.values(notes)
-          .sort((a, b) => b.date.getTime() - a.date.getTime())
-          .filter(({ text }) => {
-            if (!filter) {
-              return true;
-            }
-
-            return text.toLowerCase().includes(filter.toLowerCase());
-          })
+        {sortedNotes
+          .filter(({ text }) => !filter || text.toLowerCase().includes(filter.toLowerCase()))
           .map(({ id, text, date }) => (
             <NoteButton
               key={id}
               isActive={activeNoteId === id}
-              onNoteActivated={() => onNoteActivated(id)}
+              onNoteActivated={handleNoteActivation(id)}
               text={text}
               filterText={filter}
               date={date}
@@ -47,32 +45,12 @@ function NotesList({
 
       <div className="notes-list__controls">
         <ButtonGroup size="small">
-          <Button
-            classes={{ root: "notes-list__control" }}
-            onClick={() => onNewNotesRequested({ count: 1, paragraphs: 1 })}
-          >
-            + Note
-          </Button>
-          <Button
-            classes={{ root: "notes-list__control" }}
-            onClick={() => onNewNotesRequested({ count: 1, paragraphs: 300 })}
-          >
-            + Huge
-          </Button>
-          <Button
-            classes={{ root: "notes-list__control" }}
-            onClick={() => onNewNotesRequested({ count: 100, paragraphs: 1 })}
-          >
-            + 100
-          </Button>
+          <Button classes={{ root: "notes-list__control" }} onClick={handleNewNoteRequest(1, 1)}>+ Note</Button>
+          <Button classes={{ root: "notes-list__control" }} onClick={handleNewNoteRequest(1, 300)}>+ Huge</Button>
+          <Button classes={{ root: "notes-list__control" }} onClick={handleNewNoteRequest(100, 1)}>+ 100</Button>
         </ButtonGroup>
         <ButtonGroup size="small">
-          <Button
-            classes={{ root: "notes-list__control" }}
-            onClick={() => onDeleteAllRequested()}
-          >
-            Delete all
-          </Button>
+          <Button classes={{ root: "notes-list__control" }} onClick={onDeleteAllRequested}>Delete all</Button>
         </ButtonGroup>
       </div>
     </div>
