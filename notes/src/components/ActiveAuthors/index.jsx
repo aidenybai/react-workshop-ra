cedc46852820140458e28d915b4c519e4e3cf446
Aiden Bai <aiden.bai05@gmail.com>
2023-11-13 07:39:13 +0000

    auto-optimize: notes/src/components/ActiveAuthors/index.jsx

--- notes/src/components/ActiveAuthors/index.jsx
+++ notes/src/components/ActiveAuthors/index.jsx
@@ -4,20 +4,19 @@ import avatar1 from "./avatar1.jpg";
 import avatar2 from "./avatar2.jpg";
 import avatar3 from "./avatar3.jpg";
 
-function ActiveAuthors() {
+const ActiveAuthors = () => {
   const activeThisMonth = useSelector((state) =>
     state.users.filter(
-      (i) =>
-        new Date(i.lastActiveDate).getFullYear() === 2023 &&
-        new Date(i.lastActiveDate).getMonth() === 0
+      (user) =>
+        new Date(user.lastActiveDate).getFullYear() === 2023 &&
+        new Date(user.lastActiveDate).getMonth() === 0
     )
   );
 
   return (
     <div className="primary-pane__authors">
       <div className="primary-pane__authors-last-active">
-        {activeThisMonth.length} users active this month:{" "}
-        {activeThisMonth.map((i) => i.name).join(", ")}
+        {`${activeThisMonth.length} users active this month: ${activeThisMonth.map((user) => user.name).join(", ")}`}
       </div>
       <AvatarGroup max={2}>
         <Avatar src={avatar1} />
