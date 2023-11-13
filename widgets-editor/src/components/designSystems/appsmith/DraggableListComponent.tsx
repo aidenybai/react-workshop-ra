07b969248209d526fbc07640f002edfdfbb452c9
Aiden Bai <aiden.bai05@gmail.com>
2023-11-13 12:47:29 +0000

    auto-optimize: widgets-editor/src/components/designSystems/appsmith/DraggableListComponent.tsx

--- widgets-editor/src/components/designSystems/appsmith/DraggableListComponent.tsx
+++ widgets-editor/src/components/designSystems/appsmith/DraggableListComponent.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "constants/DefaultTheme";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+
 const ItemWrapper = styled.div`
   padding-right: 0;
   margin: 8px 0 0 0;
@@ -32,107 +33,81 @@ interface DroppableComponentProps {
   onEdit?: (index: number) => void;
 }
 
-interface DroppableComponentState {
-  items: Array<Record<string, unknown>>;
-}
+const DroppableComponent: React.FC<DroppableComponentProps> = ({
+  items,
+  renderComponent,
+  deleteOption,
+  updateOption,
+  toggleVisibility,
+  updateItems,
+  onEdit,
+}) => {
+  const [stateItems, setStateItems] = useState(items);
 
-export class DroppableComponent extends React.Component<
-  DroppableComponentProps,
-  DroppableComponentState
-> {
-  constructor(props: DroppableComponentProps) {
-    super(props);
-    this.state = {
-      items: props.items,
-    };
-  }
-
-  componentDidUpdate(prevProps: DroppableComponentProps) {
-    if (this.props.items.length !== prevProps.items.length) {
-      this.setState({ items: this.props.items });
-    } else if (
-      JSON.stringify(this.props.items) !== JSON.stringify(prevProps.items)
-    ) {
-      this.setState({ items: this.props.items });
+  useEffect(() => {
+    if (items.length !== stateItems.length || JSON.stringify(items) !== JSON.stringify(stateItems)) {
+      setStateItems(items);
     }
-  }
+  }, [items, stateItems]);
 
-  onDragEnd = (result: any) => {
+  const onDragEnd = (result: any) => {
     const { destination, source } = result;
-    const items: Array<Record<string, unknown>> = [...this.state.items];
+    const newItems = Array.from(stateItems);
     const sourceIndex = source.index;
     let destinationIndex;
     if (!destination) {
-      destinationIndex = items.length;
+      destinationIndex = newItems.length;
     } else {
-      if (
-        destination.droppableId === source.droppableId &&
-        destination.index === source.index
-      ) {
+      if (destination.droppableId === source.droppableId && destination.index === source.index) {
         return;
       }
       destinationIndex = destination.index;
     }
-    items.splice(destinationIndex, 0, items[sourceIndex]);
-    items.splice(sourceIndex + (destinationIndex < sourceIndex ? 1 : 0), 1);
-    this.setState({ items: items });
-    this.props.updateItems(items);
+    const [removed] = newItems.splice(sourceIndex, 1);
+    newItems.splice(destinationIndex, 0, removed);
+    setStateItems(newItems);
+    updateItems(newItems);
   };
 
-  render() {
-    const {
-      renderComponent,
-      deleteOption,
-      updateOption,
-      toggleVisibility,
-      onEdit,
-    } = this.props;
-    return (
-      <DragDropContext onDragEnd={this.onDragEnd}>
-        <Droppable droppableId="droppable">
-          {({ innerRef, droppableProps, placeholder }) => (
-            <DroppableWrapper
-              ref={innerRef as React.Ref<HTMLDivElement>}
-              {...droppableProps}
-            >
-              {this.state.items.map(
-                (item: { id: string } & any, index: number) => {
-                  return (
-                    <Draggable
-                      draggableId={item.id}
-                      key={item.id}
-                      index={index}
-                    >
-                      {({ innerRef, draggableProps, dragHandleProps }) => (
-                        <ItemWrapper
-                          ref={innerRef as React.Ref<HTMLDivElement>}
-                          {...draggableProps}
-                          {...dragHandleProps}
-                          style={{
-                            ...draggableProps.style,
-                            userSelect: "none",
-                            position: "static",
-                          }}
-                        >
-                          {renderComponent({
-                            deleteOption,
-                            updateOption,
-                            toggleVisibility,
-                            onEdit,
-                            item,
-                            index,
-                          })}
-                        </ItemWrapper>
-                      )}
-                    </Draggable>
-                  );
-                },
-              )}
-              {placeholder}
-            </DroppableWrapper>
-          )}
-        </Droppable>
-      </DragDropContext>
-    );
-  }
-}
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable droppableId="droppable">
+        {({ innerRef, droppableProps, placeholder }) => (
+          <DroppableWrapper
+            ref={innerRef as React.Ref<HTMLDivElement>}
+            {...droppableProps}
+          >
+            {stateItems.map((item: { id: string } & any, index: number) => (
+              <Draggable draggableId={item.id} key={item.id} index={index}>
+                {({ innerRef, draggableProps, dragHandleProps }) => (
+                  <ItemWrapper
+                    ref={innerRef as React.Ref<HTMLDivElement>}
+                    {...draggableProps}
+                    {...dragHandleProps}
+                    style={{
+                      ...draggableProps.style,
+                      userSelect: "none",
+                      position: "static",
+                    }}
+                  >
+                    {renderComponent({
+                      deleteOption,
+                      updateOption,
+                      toggleVisibility,
+                      onEdit,
+                      item,
+                      index,
+                    })}
+                  </ItemWrapper>
+                )}
+              </Draggable>
+            ))}
+            {placeholder}
+          </DroppableWrapper>
+        )}
+      </Droppable>
+    </DragDropContext>
+  );
+};
+
+export default DroppableComponent;
