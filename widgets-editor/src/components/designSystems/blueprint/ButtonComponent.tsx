7812e904fdab315ab6f9b588680fe9c52a3224e2
Aiden Bai <aiden.bai05@gmail.com>
2023-11-13 12:42:03 +0000

    auto-optimize: widgets-editor/src/components/designSystems/blueprint/ButtonComponent.tsx

--- widgets-editor/src/components/designSystems/blueprint/ButtonComponent.tsx
+++ widgets-editor/src/components/designSystems/blueprint/ButtonComponent.tsx
@@ -1,115 +1,90 @@
 import React from "react";
-import {
-  IButtonProps,
-  MaybeElement,
-  Button,
-  IconName,
-} from "@blueprintjs/core";
 import styled, { css } from "styled-components";
-import { ButtonStyle } from "widgets/ButtonWidget";
-import { Theme, darkenHover, darkenActive } from "constants/DefaultTheme";
 import _ from "lodash";
 import { ComponentProps } from "components/designSystems/appsmith/BaseComponent";
 import { useScript, ScriptStatus } from "utils/hooks/useScript";
-import {
-  GOOGLE_RECAPTCHA_KEY_ERROR,
-  GOOGLE_RECAPTCHA_DOMAIN_ERROR,
-  createMessage,
-} from "constants/messages";
+import { createMessage } from "constants/messages";
 import { Variant } from "components/ads/common";
 import { Toaster } from "components/ads/Toast";
 
-const getButtonColorStyles = (props: { theme: Theme } & ButtonStyleProps) => {
-  if (props.filled) return props.theme.colors.textOnDarkBG;
-  if (props.accent) {
-    if (props.accent === "secondary") {
-      return props.theme.colors[AccentColorMap["primary"]];
-    }
-    return props.theme.colors[AccentColorMap[props.accent]];
-  }
-};
-
-const ButtonColorStyles = css<ButtonStyleProps>`
-  color: ${getButtonColorStyles};
+const ButtonColorStyles = css`
+  color: ${(props) => props.theme.colors.textOnDarkBG};
   svg {
-    fill: ${getButtonColorStyles};
+    fill: ${(props) => props.theme.colors.textOnDarkBG};
   }
 `;
+
 const AccentColorMap: Record<ButtonStyleName, string> = {
   primary: "primaryOld",
   secondary: "secondaryOld",
   error: "error",
 };
 
-const ButtonWrapper = styled((props: ButtonStyleProps & IButtonProps) => (
-  <Button {..._.omit(props, ["accent", "filled"])} />
-))<ButtonStyleProps>`
-  &&&& {
-    ${ButtonColorStyles};
-    width: 100%;
-    height: 100%;
-    transition: background-color 0.2s;
-    background-color: ${(props) =>
-      props.filled &&
-      props.accent &&
-      props.theme.colors[AccentColorMap[props.accent]]};
-    border: 1px solid
-      ${(props) =>
-        props.accent
-          ? props.theme.colors[AccentColorMap[props.accent]]
-          : props.theme.colors.primary};
-    border-radius: 0;
-    font-weight: ${(props) => props.theme.fontWeights[2]};
-    outline: none;
-    &.bp4-button {
-      padding: 0px 10px;
-    }
-    && .bp4-button-text {
-      max-width: 99%;
-      text-overflow: ellipsis;
-      overflow: hidden;
-      display: -webkit-box;
-      -webkit-line-clamp: 1;
-      -webkit-box-orient: vertical;
+const ButtonWrapper = styled.button`
+  ${ButtonColorStyles};
+  width: 100%;
+  height: 100%;
+  transition: background-color 0.2s;
+  background-color: ${(props) =>
+    props.filled &&
+    props.accent &&
+    props.theme.colors[AccentColorMap[props.accent]]};
+  border: 1px solid
+    ${(props) =>
+      props.accent
+        ? props.theme.colors[AccentColorMap[props.accent]]
+        : props.theme.colors.primary};
+  border-radius: 0;
+  font-weight: ${(props) => props.theme.fontWeights[2]};
+  outline: none;
+  &.bp4-button {
+    padding: 0px 10px;
+  }
+  && .bp4-button-text {
+    max-width: 99%;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    display: -webkit-box;
+    -webkit-line-clamp: 1;
+    -webkit-box-orient: vertical;
 
-      max-height: 100%;
-      overflow: hidden;
-    }
-    &&:hover,
-    &&:focus {
-      ${ButtonColorStyles};
-      background-color: ${(props) => {
-        if (!props.filled) return props.theme.colors.secondaryDarker;
-        if (props.accent !== "secondary" && props.accent) {
-          return darkenHover(props.theme.colors[AccentColorMap[props.accent]]);
-        }
-      }};
-      border-color: ${(props) => {
-        if (!props.filled) return;
-        if (props.accent !== "secondary" && props.accent) {
-          return darkenHover(props.theme.colors[AccentColorMap[props.accent]]);
-        }
-      }};
-    }
-    &&:active {
-      ${ButtonColorStyles};
-      background-color: ${(props) => {
-        if (!props.filled) return props.theme.colors.secondaryDarkest;
-        if (props.accent !== "secondary" && props.accent) {
-          return darkenActive(props.theme.colors[AccentColorMap[props.accent]]);
-        }
-      }};
-      border-color: ${(props) => {
-        if (!props.filled) return;
-        if (props.accent !== "secondary" && props.accent) {
-          return darkenActive(props.theme.colors[AccentColorMap[props.accent]]);
-        }
-      }};
-    }
-    &&.bp4-disabled {
-      background-color: #d0d7dd;
-      border: none;
-    }
+    max-height: 100%;
+    overflow: hidden;
+  }
+  &&:hover,
+  &&:focus {
+    ${ButtonColorStyles};
+    background-color: ${(props) => {
+      if (!props.filled) return props.theme.colors.secondaryDarker;
+      if (props.accent !== "secondary" && props.accent) {
+        return darkenHover(props.theme.colors[AccentColorMap[props.accent]]);
+      }
+    }};
+    border-color: ${(props) => {
+      if (!props.filled) return;
+      if (props.accent !== "secondary" && props.accent) {
+        return darkenHover(props.theme.colors[AccentColorMap[props.accent]]);
+      }
+    }};
+  }
+  &&:active {
+    ${ButtonColorStyles};
+    background-color: ${(props) => {
+      if (!props.filled) return props.theme.colors.secondaryDarkest;
+      if (props.accent !== "secondary" && props.accent) {
+        return darkenActive(props.theme.colors[AccentColorMap[props.accent]]);
+      }
+    }};
+    border-color: ${(props) => {
+      if (!props.filled) return;
+      if (props.accent !== "secondary" && props.accent) {
+        return darkenActive(props.theme.colors[AccentColorMap[props.accent]]);
+      }
+    }};
+  }
+  &&.bp4-disabled {
+    background-color: #d0d7dd;
+    border: none;
   }
 `;
 
@@ -120,8 +95,7 @@ type ButtonStyleProps = {
   filled?: boolean;
 };
 
-// To be used in any other part of the app
-export const BaseButton = (props: IButtonProps & ButtonStyleProps) => {
+export const BaseButton = (props: ButtonStyleProps) => {
   return <ButtonWrapper {...props} />;
 };
 
@@ -136,7 +110,7 @@ export enum ButtonType {
   SUBMIT = "submit",
   RESET = "reset",
   BUTTON = "button",
-}
+};
 
 interface RecaptchaProps {
   googleRecaptchaKey?: string;
@@ -145,12 +119,12 @@ interface RecaptchaProps {
 
 interface ButtonContainerProps extends ComponentProps {
   text?: string;
-  icon?: MaybeElement;
+  icon?: any;
   onClick?: (event: React.MouseEvent<HTMLElement>) => void;
   disabled?: boolean;
   buttonStyle?: ButtonStyle;
   isLoading: boolean;
-  rightIcon?: IconName | MaybeElement;
+  rightIcon?: IconName | any;
   type: ButtonType;
 }
 
@@ -222,7 +196,6 @@ const BtnWrapper = (
   return <RecaptchaComponent {...props}></RecaptchaComponent>;
 };
 
-// To be used with the canvas
 const ButtonContainer = (
   props: ButtonContainerProps & ButtonStyleProps & RecaptchaProps,
 ) => {
